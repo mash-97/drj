a8dc8531a66a9081b889c063d625114a36c888ad
NiloyMallick <mallickniloy@gmail.com>
2021-12-17 11:13:57 +0600

    appointment model

--- appointment/models.py
+++ appointment/models.py
@@ -1,12 +1,18 @@
+from django.contrib.auth import get_user_model
 from django.db import models
-
-from users.models import *
 
 
 class Appointment(models.Model):
-    appointee = models.ForeignKey(Appointee, on_delete=models.CASCADE)
     employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
-    subject = models.CharField(max_length=255)
-    creation_datetime = models.DateTimeField(auto_now_add=True)
-    appointed_datetime = models.DateTimeField(null=True)
-    
+    appointee = models.ForeignKey(Appointee, on_delete=models.CASCADE)
+    date_time = models.DateTimeField(null=True)
+    subject = models.CharField(max_length=200)
+    is_pending = models.BooleanField(default=True)
+    is_checked = models.BooleanField(default=False)
+
+
+class Comment(models.Model):
+    appointment = models.ForeignKey(Appointment, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    text = models.TextField(null=True)
+    date_time = models.DateTimeField(auto_now_add=True) 
